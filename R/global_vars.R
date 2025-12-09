3954c37c8febe5114f706327d7c701d49c63edb8
Brian Helsel <bhelsel@kumc.edu>
2025-12-09 10:19:10 -0600

    Update global_vars.R

--- R/global_vars.R
+++ R/global_vars.R
@@ -25,16 +25,21 @@ if (getRversion() >= "2.15.1") {
   "enrollment_by_cycle",
   "demographics",
   "cognition_task_counts",
+  "ntgedsd_counts",
   "imaging_scan_counts",
+  "imaging_scan_counts_controls",
   "mri_sequence_counts",
+  "mri_sequence_counts_controls",
   "imaging_scan_counts_by_site",
   "consensus_primary_by_cycle",
   "healthhistory",
   "apoe",
   "karyotype",
   "karyotype_by_site",
-  "blood",
-  "csf",
+  "blood_draw_counts",
+  "blood_draw_counts_controls",
+  "csf_counts",
+  "csf_counts_controls",
   "omics"
 )
 
@@ -64,4 +69,26 @@ if (getRversion() >= "2.15.1") {
   "5" , "ASL"              , "ASL Axial 3D pCASL or Axial 3D PASL"                                               ,
   "6" , "T2 FSE"           , "T2 FSE Sagittal 3D T2 Weighted Sequence"                                           ,
   "7" , "rs-fMRI"          , "rs-fMRI Axial fcMRI (multiband if applicable)"                                     ,
+)
+
+.imaging_key <-
+  tibble::tribble(
+    ~atri_scan_names , ~loni_scan_names , ~scan_labels       ,
+    "amymeta"        , "amy"            , "Amyloid PET Scan" ,
+    "taumeta"        , "tau"            , "Tau PET Scan"     ,
+    "mrimeta"        , "mri"            , "MRI Imaging"      ,
+    "fdgmeta"        , "fdg"            , "FDG PET Scan"     ,
+  )
+
+
+# fmt: skip
+.disclosure_key <- list(
+  abcds = list(
+    demographics = c("de_eval_date", "de_gender", "age_at_visit"),
+    dsmse = c("dsto2", "dsla2", "colshbx", "colmemscr", "dsvs1"),
+    recall = c("frssa", "frssb", "crssa", "crssb"),
+    ntgedsd = c("adltot", "ntlctot", "ntswtot", "ntambtot", "ntmemtot", "ntbehtot", "ntsrptot", "ntootot", "nttotal"),
+    exam = c("ht", "htu", "wt", "wtu", "bpsys", "bpdia")
+  ),
+  trcds = list()
 )
